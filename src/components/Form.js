a408c968945f1f8f08d9b526f9178cf7994690c2
eli-alkorta <elialkor@gmail.com>
2020-06-28 22:32:58 +0200

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    <div className="Form">
+      <h1>Hola</h1>
+    </div>
+  }
+}
+
+export default Form;
