ceb4c9f297f225a21da4dcd1775a3e2d83cafc2a
eli-alkorta <elialkor@gmail.com>
2020-07-20 15:46:07 +0200

    scary chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,34 @@
 import React from 'react';
 
+// componentDidMount() {
+//   this.lightsOnOff();
+// }
+
 const Chat = (props) => {
-  
-  return(
-    <div>Hola desde chat {props.location.state.userInfo.surname}</div>
-  )
-}
+
+  lightsOnOff();
+  let click = new Audio();
+  click.src = "./audio/click-audio.mp3";
+
+  function lightsOnOff () {
+    setInterval(lightAndSound, 2300);
+  }
+
+  function lightAndSound() {
+    let chat = document.querySelector('.chat-container');
+    chat.classList.toggle('ghost');
+    chat.classList.toggle('scary');
+    createClick()
+  }
+
+  function createClick(){
+    click.play();    
+ }
+
+    return(
+    <div className="chat-container">Hola desde chat {props.location.state.userInfo.surname}</div>
+    )
+  }
+
 
 export default Chat;
