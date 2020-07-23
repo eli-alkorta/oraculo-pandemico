6612a7602b291121613faa988eb360fa973e7dd5
eli-alkorta <elialkor@gmail.com>
2020-07-23 13:06:06 +0200

    photo routes4

--- src/components/History1.js
+++ src/components/History1.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Agapita from '.../public/img/agapita.jpg';
+import Agapita from '../img/agapita.jpg';
 
 const History1 = (props) => {
   
