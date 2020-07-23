3d70f87cbf5924fe5f5bd245f580999f64bfd7ba
eli-alkorta <elialkor@gmail.com>
2020-07-23 12:39:12 +0200

    Updates

---
+++ precache-manifest.2032015bdc840629834094f4f8122cf7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5522dc608dadce31165a0f83375629d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "96983eb89020c8a0b68f",
+    "url": "./static/css/main.0c3f4a65.chunk.css"
+  },
+  {
+    "revision": "8350c5c0f489ce1ace68",
+    "url": "./static/js/2.4e07031b.chunk.js"
+  },
+  {
+    "revision": "ffa35a34964860fdc97ed79ca9abd27c",
+    "url": "./static/js/2.4e07031b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96983eb89020c8a0b68f",
+    "url": "./static/js/main.31ca7456.chunk.js"
+  },
+  {
+    "revision": "273ccd61cc0069e8e4fb",
+    "url": "./static/js/runtime-main.97b33596.js"
+  },
+  {
+    "revision": "bc010bb536499a51536418ba3af8f3a9",
+    "url": "./static/media/scary.bc010bb5.jpg"
+  }
+]);
